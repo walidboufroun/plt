12a65c6462d3880ed9c237b63ab88dc44aba1275
walidboufroun <boufrounewalid@gmail.com>
2024-05-11 17:17:48 +0100

    changes 17:17 11/05/2024

--- app/Http/Controllers/ClientControllers/EmployeeControllerClient.php
+++ app/Http/Controllers/ClientControllers/EmployeeControllerClient.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
+
 class EmployeeControllerClient extends Controller
 {
     public function index()
@@ -22,12 +23,18 @@ class EmployeeControllerClient extends Controller
         $employes = Client::where('id', '!=', $authenticatedClientId)
             ->where('id_societe', $authenticatedClientSocieteId)
             ->get();
-            
+
         return view('client.employes', compact('employes'));
     }
 
     public function addEmployee(Request $request)
     {
+        // Validate the request data
+        $request->validate([
+            //'photo' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Example validation rules for image files
+            // Add more validation rules for other fields if needed
+        ]);
+        // Create a new Client instance
         $employee = new Client();
         $employee->username = $request->input('username');
         $employee->password = $request->input('password');
@@ -37,18 +44,32 @@ class EmployeeControllerClient extends Controller
         $employee->type = $request->input('type');
         $employee->id_societe = $request->input('id_societe');
         $employee->adresse = $request->input('adresse');
-        $employee->photo = $request->input('photo');
-        $employee->save();
-        if ($request->hasFile('photo')) {
-            dd($request->file('photo'));
-        }
-        $file = $request->file('photo');
-        //$fileName = time() . '_' . $file->getClientOriginalName();
-        $filename= 'aaa.jpg';
-        $file->storeAs('images/client', $fileName);
 
+        // Get the uploaded file
+        $file = $request->file('file');
+        
+        // Check if a file was uploaded
+        if ($file) {
+            // Get the extension of the uploaded file
+            $extension = $file->getClientOriginalExtension();
+
+            // Generate a unique filename using current timestamp and extension
+            $uniqueFileName = now()->format('Ymd_His') . '.' . $extension;
+
+            // Store the file in the destination folder
+            $file->storeAs('client', $uniqueFileName);
+
+            // Assign the filename to the photo attribute of the employee
+            $employee->photo = $uniqueFileName;
+        }
+
+        // Save the employee record to the database
+        $employee->save();
+
+        // Redirect back to the Employee page
         return redirect()->route('Client-Employes');
     }
+
     public function deleteEmployee($id)
     {
         Client::find($id)->delete();
